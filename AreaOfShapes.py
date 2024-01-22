bd60830398920e9a3784b4b0ab1e7aa39a94241f
RZ Rakib <rakibzaman824@gmail.com>
2024-01-22 19:17:40 +0200

    create and add AreaOfShapes.py

---
+++ AreaOfShapes.py
@@ -0,0 +1,7 @@
+# Triangle
+TA_base = float(input("What is the base of Triangle "))
+TA_height = float(input("What is the vertical height of Triangle "))
+
+AreaOfTriangle = 1/2 * TA_base * TA_height
+
+print(AreaOfTriangle)
