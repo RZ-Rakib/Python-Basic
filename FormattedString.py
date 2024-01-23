2c18131761029f285d506f124c26c064aaf7ab88
RZ Rakib <rakibzaman824@gmail.com>
2024-01-23 11:07:17 +0200

    create and uploaded FormattedString.py

---
+++ FormattedString.py
@@ -0,0 +1,10 @@
+num1 = int(input("what is number one? "))
+num2 = int(input("what is number two? "))
+
+print(f' The sum of {num1} + {num2} = {num1 + num2}')
+
+name = input("What is your name? ")
+email = input("What is your email id? ")
+
+print(name, end=" & ")
+print(email)
