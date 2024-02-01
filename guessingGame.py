3335ff82e9ebd5b9a4bc8303a780517d9849728c
RZ Rakib <rakibzaman824@gmail.com>
2024-02-01 15:58:26 +0200

    create add guessingGame.py

---
+++ guessingGame.py
@@ -0,0 +1,24 @@
+from random import randint
+
+count = 0
+won = 0
+loss = 0
+for counts in range(1, 6):
+    userInput = int(input("Guess the number(between 1-5): "))
+    randomNumber = randint(1, 5)
+    count += 1
+    print(f"Attempt left: {5 - count}")
+    
+    if userInput == randomNumber:
+        won += 1
+        print("you are won.")
+        print(f"Total won: {won}")
+        print(f"Total loss: {loss}")
+
+    else:
+        loss += 1
+        print("You are loss.")
+        print(f"Total won: {won}")
+        print(f"Total loss: {loss}")
+
+print("Your attempts are finished. ")
