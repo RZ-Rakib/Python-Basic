f36afd0b4880caa5ec6daf47cc6297b5bfad5da3
RZ Rakib <rakibzaman824@gmail.com>
2024-03-11 17:57:22 +0200

    create abstract file

--- OOP/abstraction.py
+++ OOP/abstraction.py
@@ -1,8 +1,12 @@
+#importing Abstract base class(ABC), must have to defined abastractmethods
+from abc import ABC, abstractmethod
+
 # Super class
-class Animal:
+class Animal(ABC): #after defined a class as abstract, it is not allow to create an object
     def __init__(self, name) -> None:
         self.name = name
-
+    
+    @abstractmethod # only derived classes can override it
     def speak(self):
         pass # Abstract method, to be overridden in derived classes
 
@@ -17,6 +21,7 @@ class Cat(Animal): # inherited super class
         return f"{self.name} says Meaw!"
     
 # creating instance of derived class
+# Shape class is unable to create an instance due to defined as abstract
 dog_instance = Dog("Jack")
 cat_instance = Cat("Musta")
 
--- OOP/function/recursion.py
+++
@@ -1,7 +0,0 @@
-def recursion(n):
-    if n == 1:
-        return 1
-    else:
-        return n * recursion(n-1)
-
-print(f'The factorial is : {recursion(5)}')
