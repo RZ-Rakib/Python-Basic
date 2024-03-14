7c14b75ab0feb85e580285fee8be9aab893a350e
RZ Rakib <rakibzaman824@gmail.com>
2024-03-14 17:31:21 +0200

    made some changes with git ignore and uoloded customised module

---
+++ OOP/create_new_module/area.py
@@ -0,0 +1,5 @@
+def area_of_triangle(base, height):
+    print(f"The area of triangle is: {0.5 * base * height}")
+
+def area_of_rectangle(length, width):
+    print(f"The area of rectangle is: {length * width}")
---
+++ OOP/create_new_module/customModule.py
@@ -0,0 +1,4 @@
+from area import area_of_triangle,area_of_rectangle
+
+area_of_rectangle(5,6)
+area_of_triangle(5,6)
